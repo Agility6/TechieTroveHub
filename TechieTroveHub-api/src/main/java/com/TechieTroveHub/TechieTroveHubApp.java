a84eef5a8a0656961e71c82708bda806e3c8a623
Agility6 <agility1013@gmail.com>
2024-03-14 21:03:50 +0800

    chore: init and add common springboot env

---
+++ TechieTroveHub-api/src/main/java/com/TechieTroveHub/TechieTroveHubApp.java
@@ -0,0 +1,20 @@
+package com.TechieTroveHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName: TechieTroveHubApp
+ * Description:
+ *
+ * @Author agility6
+ * @Create 2024/3/14 16:48
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class TechieTroveHubApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechieTroveHubApp.class, args);
+    }
+}
